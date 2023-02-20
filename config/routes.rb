5659b3c46f4be5d499b0fddc751e36cfe2047f8c
Navjot Singh <navjot_@outlook.com>
2023-02-21 02:05:44 +0530

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #root 'application#hello'#application here is the name of the controller, and hello is the action or method
   root 'pages#home'
+  get 'about', to: 'pages#about'
   #can make this controller using 'rails generate controller pages'
 end
